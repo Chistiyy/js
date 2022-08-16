8606dd99a458e2a3dd2dcac52f1f682601a2dc88
Deni Tataev <d.tataev@gmail.com>
2022-08-16 19:01:46 +0300

    Первый урок по Java

---
+++ index.js
@@ -0,0 +1,38 @@
+// console.log("Hello, World")
+// console.log(2+3)
+// console.log(10-2);
+// console.log(2*3)
+// console.log(10/5);
+// console.log(2**3);
+// console.log((2+3)+(10-2)+(2*3)+(10/5)+(2**3));
+
+// let, const
+// let n1 = 12
+// let n2 = 14
+// let n3 = 18
+// let n4 = 37
+// let n5 = 28394
+// let str1 = "Poop" 
+// let str2 = "doode" 
+// let str3 = "joke" 
+// let str4 = "rabbit" 
+// let str5 = "cat" 
+// console.log(str5, str1, str2)
+// console.log(n3, n4, n5)
+// n3 = 100
+// n4 = 200
+// n5 = 300
+// str2 = "Zatura"
+// str1 = "StarWars"
+// str5 = "ObiWanKinobi"
+// console.log(n3, n4, n5)
+// console.log(str1, str2, str5)
+// let arr1 = [1,2,3,4,5,-10,2]
+// console.log(arr1)
+
+let names = ["Gadji", "Hasbik", "Khabib", "Zabit", "Abdulmanap"]
+console.log(names)
+let nameAge = [ 25, 20, 34, 27, 50]
+console.log (names [0])
+console.log("Имя:", names[0], ",", "Возраст", nameAge[0])
+
