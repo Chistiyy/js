15e4633ddd7ebc9e0f2c5f030ca10369d9e49022
Deni Tataev <d.tataev@gmail.com>
2022-08-22 23:10:12 +0300

    проверка джава

--- index.js
+++ index.js
@@ -30,9 +30,154 @@
 // let arr1 = [1,2,3,4,5,-10,2]
 // console.log(arr1)
 
-let names = ["Gadji", "Hasbik", "Khabib", "Zabit", "Abdulmanap"]
-console.log(names)
-let nameAge = [ 25, 20, 34, 27, 50]
-console.log (names [0])
-console.log("Имя:", names[0], ",", "Возраст", nameAge[0])
+// let names = ["Gadji", "Hasbik", "Khabib", "Zabit", "Abdulmanap"]
+// console.log(names)
+// let nameAge = [ 25, 20, 34, 27, 50]
+// console.log (names [0])
+// console.log("Имя:", names[0], ",", "Возраст", nameAge[0])
+
+// let myName = "Deni"
+// let age = 24
+// let myFriedsName = "Ramzan"
+// let car = "Mercedes"
+// let myCar = car
+// console.log ("Моя машина", car, "такая же как у", myFriedsName + ".", "Его машина", car)
+
+// let names = [
+//     ["Deni", 24],
+//     ["movsar", 32],
+//     ["Milana", 39],
+//     ["Ramzan", 29],
+//     ["Mohmad", 25]
+// ]
+// console.log("My name is " + names[0][0] + ". I am " + names[0][1])
+// console.log("My name is " + names[1][0] + ". I am " + names[1][1])
+// console.log("My name is " + names[2][0] + ". I am " + names[2][1])
+// console.log("My name is " + names[3][0] + ". I am " + names[3][1])
+// console.log("My name is " + names[4][0] + ". I am " + names[4][1])
+
+// let str = "I live in Grozny"
+// console.log(str[0])
+// console.log(str[2])
+// console.log(str[3])
+// console.log(str[4])
+// console.log(str[5])
+// console.log(str[7])
+// console.log(str[8])
+// console.log(str[10])
+// console.log(str[11])
+// console.log(str[12])
+// console.log(str[13])
+// console.log(str[14])
+// console.log(str[15])
+
+// let index = 0 
+// let str = "Deni"
+// console.log(str[index])
+// console.log(str[index + 1])
+// console.log(str[index + 2])
+// console.log(str[index + 3])
+
+// let index = 0
+// let religion = "ISLAM"
+// console.log(religion[index++])
+// console.log(religion[index++])
+// console.log(religion[index++])
+// console.log(religion[index++])
+// console.log(religion[index++])
+
+// === - сравнение
+// !== - не равно 
+// = - присвоение 
+
+// let str = "This is very long string. I dont want to console it"
+
+// let str = "ПЕРЕМЕННАЯ"
+// let arr = ["ПЕРЕМЕННАЯ"]
+// console.log("Длина строки str:", "=",   str.length)
+// console.log("Длина массива arr", "=", arr.length)
+
+// console.log(3<5)
+// console.log(3>5)
+// console.log(3<=3)
+// console.log(3>=2)
+// console.log("ab" === "ab")
+// console.log("ad" !== "ab")
+// console.log("ad" === "ad")
+
+// if (3 > 5) {
+//     console.log("first if")
+// } else if ((3+4) / 2 > 5) {
+//     console.log("second if")
+// } else if (2 > 10) {
+//     console.log("third if")
+// }
+
+// let age = 10 
+// let myName = "Ginger"
+// let horror =  "Ужас"
+// let actionMovie = "Боевик"
+// let cartoon = "Мультик"
+
+
+// if (age >= 18) {
+//     console.log("Вам " + age + " лет, поэтому можно смотреть " + horror + "жанр.")
+// } else if (age >= 12) {
+//     console.log("Вам " + age + " лет, поэтому можно смотреть " + actionMovie + "жанр.")
+// } else {
+//     console.log("Вам " + age + " лет, поэтому смотреть можно " + cartoon + " жанр.")
+// }
+
+// let index = 0
+// let home = "Chechnya"
+// console.log(home[index++])
+// console.log(home[index++])
+// console.log(home[index++])
+// console.log(home[index++])
+// console.log(home[index++])
+// console.log(home[index++])
+// console.log(home[index++])
+// console.log(home[index++])
+
+// let s = "Chechnya"
+
+// for (let i = 0; i < s.length; i++) {
+//     console.log(s[i])
+
+// }
+
+// написать цикл от 21 до 122 
+// 21
+//23
+//25
+//...
+
+// for (let i = 21; i < 122; i+=2){
+//     console.log(i)
+// }
+
+// for (let i = 2; i < 37; i++) {
+//     if(i % 2 === 0) {
+//         console.log(i)
+//     }
+// }
+
+
+// for (let i = 2; i < 37; i++) {
+//     if(i % 2 === 1) {
+//         console.log(i)
+//     }
+// }
+
+// for (let i = 0; i <= 100; ++i) {
+//     if(i % 2 === 0) {
+//         console.log("четное")
+//     } else {
+//         console.log("нечетное")
+//     }
+// }
+
+// && - и - в if должно быть true  с обоих сторон 
+// || - или в if должно быть true  с одной стороны 
+
 
